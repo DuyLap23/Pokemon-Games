822f6729ff18470f22f3bb65dfecfeecee105015
DuyLap <hoangduylap124@gmail.com>
2024-08-11 13:57:51 +0700

    secon commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,11 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/vue3-essential", "@vue/prettier"],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: "babel-eslint",
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // Các quy tắc của bạn
   },
 };
